e56b2dbff1672837f1334ab71a050f43e9295125
Sahil Dhanjal <Sahildhanjal01@gmail.com>
2022-10-28 22:18:00 +0530

    Adding a New Code cpp file

---
+++ Lecture 54 - Introduction to Stacks/2_ImplementingStacksUsingLinkedLists.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"\nHello "<<name<<endl;
+    return 0;
+}
